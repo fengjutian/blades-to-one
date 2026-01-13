922a8cc176c6d3b1d79613e24405fcdf5e26e83a
fengjutian <fjutian@foxmail.com>
2026-01-13 16:34:08 +0800

    feat(聊天界面): 实现侧边栏菜单项的选中状态切换功能
    添加 selectedComponent 函数来处理菜单项的点击事件，使用 useState 管理菜单项状态

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import styles from './chat.module.scss';
 // import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 // import { AppSidebar } from "./AppSidebar";
 import { SquarePen, Image  } from 'lucide-react';
 
-
-const siderBarList = [
-  {
-    icon: <SquarePen size={18} />,
-    name: '新聊天',
-    isActivity: true
-  },
-  {
-    icon: <Image size={18} />,
-    name: '图片',
-    isActivity: false
-  }
-]
-
 const Chat: React.FC = () => {
+
+  const [siderBarList, setSiderBarList] = useState<{ icon: ReactElement<any, any>; name: string; isActivity: boolean; }[]>([
+    {
+      icon: <SquarePen size={18} />,
+      name: '新聊天',
+      isActivity: true
+    },
+    {
+      icon: <Image size={18} />,
+      name: '图片',
+      isActivity: false
+    }
+  ]);
+
+  const selectedComponent = (componentName: string) => {
+    setSiderBarList(prevList =>
+      prevList.map(item => ({
+        ...item,
+        isActivity: item.name === componentName
+      }))
+    );
+  }
 
 
   return (
@@ -26,7 +34,8 @@ const Chat: React.FC = () => {
       <div className={styles.sidebar}>
         {
           siderBarList.map((item) => (
-            <div key={item.name} onClic className={`${styles.siderBarItem} ${item.isActivity ? styles.activity : ''}`}>
+            <div key={item.name} onClick={() => selectedComponent(item.name)}
+              className={`${styles.siderBarItem} ${item.isActivity ? styles.activity : ''}`}>
               {item.icon}
               <div className={styles.siderBarItemName}>{item.name}</div>
             </div>
